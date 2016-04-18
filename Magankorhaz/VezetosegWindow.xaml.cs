909c72da8ad0c62a6fcca87988aeffd3029dc961
JaszyKitti <jaszykitti@gmail.com>
2016-04-18 21:59:04 +0200

    Vezetőség ablak 80%-ban kész Ügyintezők Számla kiállítás ablaka 80%-ban kész

--- Magankorhaz/VezetosegWindow.xaml.cs
+++ Magankorhaz/VezetosegWindow.xaml.cs
@@ -33,5 +33,26 @@ namespace Magankorhaz
         {
             Application.Current.MainWindow.Visibility = Visibility.Visible;
         }
+
+        private void attekintesbutton_click(object sender, RoutedEventArgs e)
+        {
+            koltsegvetesikimutatasokmenu.Visibility = System.Windows.Visibility.Hidden;
+            osztalyokkihasznaltsagamenu.Visibility = System.Windows.Visibility.Hidden;
+            attekintesmenu.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void ktskimutatasokbutton_click(object sender, RoutedEventArgs e)
+        {
+            koltsegvetesikimutatasokmenu.Visibility = System.Windows.Visibility.Visible;
+            osztalyokkihasznaltsagamenu.Visibility = System.Windows.Visibility.Hidden;
+            attekintesmenu.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void osztalyokkihasznbutton_click(object sender, RoutedEventArgs e)
+        {
+            koltsegvetesikimutatasokmenu.Visibility = System.Windows.Visibility.Hidden;
+            osztalyokkihasznaltsagamenu.Visibility = System.Windows.Visibility.Visible;
+            attekintesmenu.Visibility = System.Windows.Visibility.Hidden;
+        }
     }
 }
