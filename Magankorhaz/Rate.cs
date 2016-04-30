b46e580a6f17635eedc52bf822dea7eb93ac71fd
Molnar Barnabas <molnarbarney@gmail.com>
2016-04-30 20:49:26 +0200

    Merge remote-tracking branch 'refs/remotes/origin/master' into Márk-része
    # Conflicts:
    #	Magankorhaz.v12.suo
    #	Magankorhaz/bin/Debug/Magankorhaz.exe
    #	Magankorhaz/bin/Debug/Magankorhaz.pdb
    #	Magankorhaz/obj/Debug/DesignTimeResolveAssemblyReferences.cache
    #	Magankorhaz/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    #	Magankorhaz/obj/Debug/Magankorhaz.csproj.FileListAbsolute.txt
    #	Magankorhaz/obj/Debug/Magankorhaz.csproj.GenerateResource.Cache
    #	Magankorhaz/obj/Debug/Magankorhaz.csprojResolveAssemblyReference.cache
    #	Magankorhaz/obj/Debug/Magankorhaz.exe
    #	Magankorhaz/obj/Debug/Magankorhaz.g.resources
    #	Magankorhaz/obj/Debug/Magankorhaz.pdb
    #	Magankorhaz/obj/Debug/Magankorhaz_MarkupCompile.cache
    #	Magankorhaz/obj/Debug/Magankorhaz_MarkupCompile.i.cache
    #	Magankorhaz/obj/Debug/Magankorhaz_MarkupCompile.lref
    #	Magankorhaz/obj/Debug/MainWindow.g.cs
    #	Magankorhaz/obj/Debug/MainWindow.g.i.cs
    #	Magankorhaz/obj/Debug/OrvosWindow.baml
    #	Magankorhaz/obj/Debug/PaciensWindow.baml
    #	Magankorhaz/obj/Debug/PaciensWindow.g.cs
    #	Magankorhaz/obj/Debug/PaciensWindow.g.i.cs
    #	Magankorhaz/obj/Debug/UgyintezoWindow.g.cs
    #	Magankorhaz/obj/Debug/UgyintezoWindow.g.i.cs
    #	Magankorhaz/obj/Debug/UserControlok/OrvosIdopontModositasa.baml
    #	Magankorhaz/obj/Debug/UserControlok/OrvosSajatAdatok.baml
    #	Magankorhaz/obj/Debug/UserControlok/OrvosUjIdopontFelvetele.baml

---
+++ Magankorhaz/Rate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magankorhaz
+{
+    class Rate
+    {
+        public DateTime Date { get; set; } //2016-04-19
+        public string Curr { get; set; } //JPY
+        public double Value { get; set; } //246,7
+        public double Osszeg { get; set; }
+        public Rate(string newdate, string newcurr, string newvalue, string newosszeg)
+        {
+            Date = DateTime.Parse(newdate);
+            Curr = newcurr;
+            Value = double.Parse(newvalue);
+            Osszeg = Math.Round(double.Parse(newosszeg) / Value, 2);
+        }
+    }
+}
